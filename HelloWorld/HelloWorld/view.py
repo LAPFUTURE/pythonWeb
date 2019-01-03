35e1cec2b2c01816cf3be9e4faef1ba15842a16b
LAP <2300820209@qq.com>
2019-01-03 20:31:55 +0800

    处理上传图片

--- HelloWorld/HelloWorld/view.py
+++ HelloWorld/HelloWorld/view.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 from TestModel.models import Test
 from UserModel.models import User
 from BlogModel.models import Blog
+from django.http import JsonResponse
 from ResourceClassModel.models import ResourceClass
 
+from django.conf import settings
+import sys
+sys.path.append(r"D:\Coding\1\fengge\xiangmu\xiangmu\HelloWorld\ceshi")
+ 
+import eval
 
 def index(request):
     context = {}
@@ -46,4 +52,24 @@ def download(request):
         id = request.GET.get('id')
         response['id'] = id
     return response
+
+def handlepicture(request):
+    # print(request.FILES['image'])
+    # arr = [request.POST['styles']+'.ckpt-done',request.FILES['image']]
+    f1 = request.FILES['image']
+    fname = '%s/%s' % (settings.MEDIA_ROOT, f1.name)
+    picname = fname.split(".")[0]+".jpg"
+    with open(picname, 'wb') as pic:
+        for c in f1.chunks():
+            pic.write(c)
+    print("picture OK",picname)
+
+    post_pic = str(picname.split("/")[3])
+
+    arr = [request.POST['styles']+'.ckpt-done','../HelloWorld/ceshi/generated/'+post_pic]
+    # eval.main(arr)
+    obj = {"url":'../HelloWorld/ceshi/generated/res.jpg',"post":request.POST['styles'],"post_pic":post_pic,"picname":picname}
+    # obj = {"url":'../HelloWorld/ceshi/generated/'+post_pic,"post":request.POST['styles'],"post_pic":post_pic,"picname":picname}
+
+    return JsonResponse(obj)
 
